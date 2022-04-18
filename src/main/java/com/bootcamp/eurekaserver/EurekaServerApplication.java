860c6b3472fa299a32e01a0815ae966c52e66c8e
samuel14luis <luismendoza.samuel@gmail.com>
2022-04-18 12:54:13 -0500

    base eureka server working

--- src/main/java/com/bootcamp/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/bootcamp/eurekaserver/EurekaServerApplication.java
@@ -1,10 +1,33 @@
 package com.bootcamp.eurekaserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
+import java.util.logging.Logger;
+
+@EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class EurekaServerApplication implements CommandLineRunner {
+
+	private static final Logger logger = Logger.getLogger(EurekaServerApplication.class.toString());
+
+	@Autowired
+	private Environment env;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		//logger.log(Level.INFO, env.getProperty("spring.application.name"));
+		logger.info("Java version: " + env.getProperty("java.version"));
+		logger.info("Application name: " + env.getProperty("spring.application.name"));
+		logger.info("Properties file upload status: " + env.getProperty("my-own-app.properties.status"));
+		logger.info("Eureka Server on : http://" + env.getProperty("eureka.instance.hostname") + ":" + env.getProperty("server.port") );
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
